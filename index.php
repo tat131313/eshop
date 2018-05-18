a3cb1cde3c7a0cab4c089e8696a5a0cbf72f79df
tat131313 <tatyapleshka@gmail.com>
2018-05-18 14:51:12 +0300

    first

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>E-shop</title>
+    </head>
+    <body>
+        <form method="POST">
+            <div align="center">
+                <input name="name" placeholder="Enter your name: "></input> 
+            </div>
+
+            <div align="center">
+                <input type="submit" value="Submit"/>
+            </div>
+        </form>
+    </body>
+</html>
